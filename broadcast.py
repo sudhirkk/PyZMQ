90bf5053643281dde34a3becbc0d8b2c994d4ca1
Sudhir Krishnan <sudhirkk@gmail.com>
2014-09-13 10:18:22 -0400

    Updated to an object-based implimentation

--- broadcast.py
+++ broadcast.py
@@ -1,19 +1,47 @@
 import zmq
-
-from random import randrange
 import time
+import sys
+from random import randrange
 
-context = zmq.Context()
 
-socket = context.socket(zmq.PUB)
-socket.bind('tcp://*:5556')
+class Weather:
+	def __init__(self):
+		self.zipcode     = 0
+		self.temperature = 0
+		self.humidity    = 0
 
-while True:
-	time.sleep(0.1)
-	print('broadcasting ...')
-	zipcode = randrange(1, 100000)
-	temperature = randrange(-80, 135)
-	relhumidity = randrange(10, 60)
-	socket.send_string(('%i %i %i') % (zipcode, temperature, relhumidity))
+	def init(self):
+		self.zipcode     = randrange(1, 100000)
+		self.temperature = randrange(-80, 135)
+		self.humidity    = randrange(10, 60)
+	
+
+class BroadcastServer:
+	def __init__(self, port):
+		self.context = zmq.Context()
+		self.port    = port
+		self.socket  = self.context.socket(zmq.PUB)
+		self.weather = Weather()
+	
+	def publish(self):
+		self.weather.init()
+		self.socket.send_string(('%i %i %i') % (self.weather.zipcode, \
+			self.weather.temperature, self.weather.humidity))
+		
+	def start(self):
+		self.socket.bind('tcp://*:' + str(self.port))
+		while True:
+			time.sleep(0.1)
+			self.publish()
+					
+
+
+def main():
+	port = sys.argv[1]
+	server = BroadcastServer(port)
+	server.start()
+	
+if __name__ == '__main__':
+	sys.exit(main())
 
 
